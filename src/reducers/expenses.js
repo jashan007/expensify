b1a7494e11fdc9ce889f1c609fffc9b4a8f35995
jashan007 <jd38800@gmail.com>
2021-07-30 13:47:55 +0530

    added demo state

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -9,8 +9,16 @@ export default (state = expensesReducerDefaultState, action) => {
         ...state,
         action.expense
       ];
+      //----------------OR-------------
+      //return state.concat(action.expense);
+
+      //action.push() changes current state which is not good .but action.concat and ...state returns new array.
+      // It do not manipulate current state.But prefer spread operator then .Refer to Spread operator video
     case 'REMOVE_EXPENSE':
       return state.filter(({ id }) => id !== action.id);
+      //filter returns new array.
+      //Or
+      //return state.filter((expenses.id)=> { return expenses.id !== action.id } )
     case 'EDIT_EXPENSE':
       return state.map((expense) => {
         if (expense.id === action.id) {
@@ -24,5 +32,27 @@ export default (state = expensesReducerDefaultState, action) => {
       });
     default:
       return state;
+  }
+};
+
+
+
+
+
+//for referene only..means for imagining how state looks inside the mind
+
+const demoState = {
+  expenses: [{
+    id: 'poijasdfhwer',
+    description: 'January Rent',
+    note: 'This was the final payment for that address',
+    amount: 54500,
+    createdAt: 0
+  }],
+  filters: {
+    text: 'rent',
+    sortBy: 'amount', // date or amount
+    startDate: undefined,
+    endDate: undefined
   }
 };
--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -39,3 +39,22 @@ export default (state = filtersReducerDefaultState, action) => {
       return state;
   }
 };
+
+
+//for referene only..means for imagining how state looks inside the mind
+
+const demoState = {
+  expenses: [{
+    id: 'poijasdfhwer',
+    description: 'January Rent',
+    note: 'This was the final payment for that address',
+    amount: 54500,
+    createdAt: 0
+  }],
+  filters: {
+    text: 'rent',
+    sortBy: 'amount', // date or amount
+    startDate: undefined,
+    endDate: undefined
+  }
+};
